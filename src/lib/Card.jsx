b6693fc1ea7acb7f526f17e808fb4a8acedb7f1f
toqvist <tobias-ahlqvist@outlook.com>
2022-04-02 15:44:01 +0200

    Added card flip animation

--- src/lib/Card.jsx
+++ src/lib/Card.jsx
@@ -1,31 +1,43 @@
 import React, { useState } from 'react'
+import { useSpring, animated } from '@react-spring/web'
 
-export default function Card({card, toggleSelected}) {
+export default function Card({ card, toggleSelected }) {
 
-    function handleSelected (){
+    function handleSelected() {
         toggleSelected(card.id)
     }
 
+    const flip = useSpring({
+
+        to: {
+            // opacity: !card.isSelected ? 0 : 1,
+            transform: `perspective(600px) rotateY(${!card.isSelected ? 180 : 0}deg)`,
+            config: { mass: 5, tension: 500, friction: 80 },
+        }
+    })
+
+
+
     return (
 
-        // <div className={`card ${card.isSelected && 'selected'}`} onClick={handleSelected}>
-        <div onClick={handleSelected}
-        className={`selectable ${card.isRemoved && 'removed-card'}` } 
+        <animated.div onClick={handleSelected}
+            className={`selectable ${card.isRemoved && 'removed-card'}`}
+            style={flip}
         >
-            
+
             {!card.isSelected &&
-                <img src="/src/assets/square-card.svg" alt="" width='150' height='150'/>
+                <img src="/src/assets/square-card.svg" alt=""
+                    width='150' height='150'
+                />
             }
-            
+
             {card.isSelected &&
                 <div className='card'>
                     <p>{card.icon}</p>
                 </div>
             }
-        </div>
-        // <div>isSelected is {card.isSelected}</div>
+        </animated.div>
 
-        
     )
 }
 
